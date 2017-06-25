e3a506c7c20fde0e0d21460271505868db18a74e
yuki.pro <yuki@yukiproui-MacBook-Pro.local>
2017-06-25 11:18:09 +0900

    브랜치 테스트

---
+++ TSG_fireFun/1.swift
@@ -0,0 +1,21 @@
+//
+//  1.swift
+//  TSG_fireFun
+//
+//  Created by yuki.pro on 2017. 6. 25..
+//  Copyright © 2017년 yuki. All rights reserved.
+//
+
+import UIKit
+
+class _: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
